bbf87a10b4775999855b4cde57f91f5b73a4cc2d
Leonardo Toledo <leotoledo00@gmail.com>
2020-04-19 17:51:20 -0300

    Leitura de arquivo finalizada

--- lexico.cpp
+++ lexico.cpp
@@ -5,12 +5,28 @@
 
 using namespace std;
 
-int main () {
+string le_arquivo(const string &file_name);  //Função que lerá o arquivo inicial
+
+int main () 
+{
+  string programa = le_arquivo("meu_programa.txt");
+  cout << programa << endl;
+  return 0;
+}
+
+string le_arquivo(const string &file_name)
+{
+  //Convertendo a string para char
+  char file_nameAux[file_name.size() + 1];
+  file_name.copy(file_nameAux, file_name.size() + 1);
+  file_nameAux[file_name.size()] = '\0';
+
   vector<string> line;  //String que lerá o arquivo
   string programa = ""; //String que guarda o programa inteiro em um só elemento
   string lineAux;
 
-  ifstream myfile ("meu_programa.txt"); 
+  
+  ifstream myfile (file_nameAux); 
 
   if (myfile.is_open())    //Checa se a pasta foi aberta com sucesso
   {
@@ -20,12 +36,10 @@ int main () {
 		  programa += lineAux;
     }
 
-	cout << programa << endl;
-
     myfile.close();
+    return programa;
   }
 
   else cout << "Unable to open file"; 
-
-  return 0;
+  return "ERRO"; //Retorna -1 caso dê errado
 }
