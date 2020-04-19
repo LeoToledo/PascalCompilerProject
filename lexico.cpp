7132b88dbe226f731619e3004721fd818347bbc4
Leonardo Toledo <leotoledo00@gmail.com>
2020-04-19 16:18:35 -0300

    Lendo Arquivos

--- lexico.cpp
+++ lexico.cpp
@@ -1,9 +1,31 @@
 #include<iostream>
+#include <fstream>
+#include <string>
 
 using namespace std;
 
-int main()
-{
-	cout >> "AOBA" >> endl;
-	return 0;
+int main () {
+  string line[];
+  ifstream myfile ("init_code.txt"); 
+
+  if (myfile.is_open())    //Checa se a pasta foi aberta com sucesso
+  {
+	int i = 0;
+    while (! myfile.eof() ) //enquanto end of file for false continua
+    {
+    	getline (myfile,line[i]); // como foi aberto em modo texto(padrão)
+                             //e não binário(ios::bin) pega cada linha
+		i++;
+    }
+	for(i = 0; i < len(i); i++)
+	{
+		cout << line << endl;
+	}
+	
+    myfile.close();
+  }
+
+  else cout << "Unable to open file"; 
+
+  return 0;
 }
